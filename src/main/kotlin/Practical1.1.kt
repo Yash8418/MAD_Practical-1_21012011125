026c7d6da5a6beb4f58c5a792fd0c3789d9fb57b
student <student@gnu.ac.in>
2023-07-17 10:41:14 +0530

    first practical

--- src/main/kotlin/Practical1.1.kt
+++ src/main/kotlin/Practical1.1.kt
@@ -1,4 +1,20 @@
+import javax.xml.crypto.dsig.spec.HMACParameterSpec
+
 fun main()
 {
-    println("Hello world")
+    var a:Int = 22
+    var b:Float = 1.5F
+    var c:Char = 'T'
+    var d:String = "HMS"
+    var e:Boolean = true
+    var f:Double = 96.36
+    var g:Long = 338927847
+    var h:Short = -2
+    var i:Byte = 127
+
+    println("$a is a integer")
+    println("$b is a Float")
+    println("$c is a Char")
+
+
 }
