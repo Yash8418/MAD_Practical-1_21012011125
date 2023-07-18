b9571ae59a3a0efa057b02279bbd016324dd3842
student <student@gnu.ac.in>
2023-07-18 09:49:54 +0530

    1.1,1.2,1.3

---
+++ src/main/kotlin/Practical1.4.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main()
+{
+    val sc = Scanner(System.`in`)
+    println("Enter Number:")
+    val r=sc.nextInt()
+    if(r%2==0){
+        println("Number is even")
+    }
+    else{
+        println("Number is odd")
+    }
+
+
+}
