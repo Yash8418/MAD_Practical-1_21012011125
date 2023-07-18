e8a6b57c9b93400cbb7e02bcdb87723717211694
student <student@gnu.ac.in>
2023-07-18 09:58:50 +0530

    1.1,1.2,1.3,1.4,1.5

---
+++ src/main/kotlin/Practical1.5.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+fun main()
+{
+
+    println("Enter Month:")
+    val month = readLine()?.toInt()!!
+
+    when(month)
+    {
+        1-> print("January")
+        2-> print("February")
+        3-> print("March")
+        4-> print("April")
+        5-> print("May")
+        6-> print("June")
+        7-> print("July")
+        8-> print("August")
+        9-> print("September")
+        10-> print("October")
+        11-> print("November")
+        12-> print("December")
+
+
+    }
+}
